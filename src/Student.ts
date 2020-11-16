8b06f99ef4e5711d3fb4997026d1b7c3d76a4f26
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-11-16 13:02:50 +0100

    more strict config

--- src/Student.ts
+++ src/Student.ts
@@ -3,17 +3,18 @@ import { School } from "./School"
 interface IStudent {
   name: string
   age: number
-  school: School
+  school: School | null
 }
 
 export class Student implements IStudent {
   name: string
   age: number
-  school: School
+  school: School | null
 
   constructor(name: string, age: number) {
     this.name = name
     this.age = age
+    this.school = null
   }
   addSchool(school: School): this {
     this.school = school
